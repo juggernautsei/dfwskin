182561b7e775d02cb6e122243f84f56ebd086a2c
juggernautsei <sherwin@affordablecustomehr.com>
2022-10-31 07:19:35 -0400

    Correcting bootstrap and adding nickname for report - displaying data

--- interface/reports/daily_summary_report.php
+++ interface/reports/daily_summary_report.php
@@ -168,6 +168,7 @@ $selectedProvider = isset($_POST['form_provider']) ? $_POST['form_provider'] : "
 
         // fetch all facility from the table
         $facilityRecords = $facilityService->getAllFacility();
+        var_dump($facilityRecords);
         foreach ($facilityRecords as $facilityList) {
             if (1 === $facilitySet && $facilityList['id'] == $selectedFacility) {
                 $facilities[$facilityList['id']] = $facilityList['name'];
@@ -356,7 +357,7 @@ $selectedProvider = isset($_POST['form_provider']) ? $_POST['form_provider'] : "
                                     <tr>
                                         <td><?php echo text(oeFormatShortDate($date)); ?></td>
                                         <!-- ALB Display facility nickname here instead of name -->
-                                        <td><?php var_dump($dataValue); echo text($dataValue['facility_nickname']); ?></td>
+                                        <td><?php echo text($facility); ?></td>
                                         <td><?php echo text($provider); ?></td>
                                         <td><?php echo isset($information['appointments']) ? text($information['appointments']) : 0; ?></td>
                                         <td><?php echo isset($information['newPatient']) ? text($information['newPatient']) : 0; ?></td>
