e5fece673121e3641be59f81238c7f122be744ce
juggernautsei <sherwin@affordablecustomehr.com>
2022-10-31 10:24:08 -0400

    second attempt to get information

--- interface/reports/daily_summary_report.php
+++ interface/reports/daily_summary_report.php
@@ -348,7 +348,6 @@ $selectedProvider = isset($_POST['form_provider']) ? $_POST['form_provider'] : "
                 <?php
                 if (count($dailySummaryReport) > 0) { // check if daily summary array has value
                     foreach ($dailySummaryReport as $date => $dataValue) { //   daily summary array which consists different/dynamic values
-                        echo "<pre>"; var_dump($dataValue); echo "</pre>";
                         foreach ($facilities as $facility) { // facility array
                             if (isset($dataValue[$facility])) {
                                 foreach ($dataValue[$facility] as $provider => $information) { // array which consists different/dynamic values
@@ -357,7 +356,7 @@ $selectedProvider = isset($_POST['form_provider']) ? $_POST['form_provider'] : "
                                     <tr>
                                         <td><?php echo text(oeFormatShortDate($date)); ?></td>
                                         <!-- ALB Display facility nickname here instead of name -->
-                                        <td><?php echo text($facility); ?></td>
+                                        <td><?php echo text($dataValue['nickname']); ?></td>
                                         <td><?php echo text($provider); ?></td>
                                         <td><?php echo isset($information['appointments']) ? text($information['appointments']) : 0; ?></td>
                                         <td><?php echo isset($information['newPatient']) ? text($information['newPatient']) : 0; ?></td>
