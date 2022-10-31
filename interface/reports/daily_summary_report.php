e90a3ee72e09611c6a25a60f3ea2e8320b972ccd
juggernautsei <sherwin@affordablecustomehr.com>
2022-10-31 10:01:56 -0400

    second attempt to get information

--- interface/reports/daily_summary_report.php
+++ interface/reports/daily_summary_report.php
@@ -162,7 +162,7 @@ $selectedProvider = isset($_POST['form_provider']) ? $_POST['form_provider'] : "
 
         // define all the variables as initial blank array
         $facilities = $totalAppointment = $totalNewPatient = $totalVisit = $totalPayment = $dailySummaryReport = $totalPaid = array();
-
+        $facility_nickname = '';
         // define all the where condition variable as initial value set 1=1
         $whereTotalVisitConditions = $whereTotalPaymentConditions = $wherePaidConditions = $whereNewPatientConditions = '1 = 1 ';
 
@@ -171,10 +171,12 @@ $selectedProvider = isset($_POST['form_provider']) ? $_POST['form_provider'] : "
         foreach ($facilityRecords as $facilityList) {
             if (1 === $facilitySet && $facilityList['id'] == $selectedFacility) {
                 $facilities[$facilityList['id']] = $facilityList['name'];
+                $facility_nickname = $facilityList['nickname'];
             }
 
             if (empty($selectedFacility)) {
                 $facilities[$facilityList['id']] = $facilityList['name'];
+                $facility_nickname = $facilityList['nickname'];
             }
         }
 
