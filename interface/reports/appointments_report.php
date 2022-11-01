3d1b1736e079f00706a4991c51ec49182be2d029
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-01 01:40:15 -0400

    Finding all the pieces.

--- interface/reports/appointments_report.php
+++ interface/reports/appointments_report.php
@@ -597,7 +597,7 @@ if (!empty($_POST['form_refresh']) || !empty($_POST['form_orderby'])) {
     <div style='float;margin-top:5px'>
         <a href='javascript:;' class='btn btn-primary btn-transmit'  onclick='checkAll(true)'><span><?php echo xlt('Select All'); ?></span></a>
         <a href='javascript:;' class='btn btn-primary btn-transmit'  onclick='checkAll(false)'><span><?php echo xlt('Clear All'); ?></span></a>
-        <a href='javascript:;' class='btn btn-primary btn-transmit' onclick='$("#form_progress2").attr("value","true"); $("#theform").submit();'>
+        <a href='javascript:;' class='btn btn-primary btn-transmit' onclick='$("#form_progress").attr("value","true"); $("#theform").submit();'>
             <span><?php echo xlt('Generate Progress Notes'); ?></span>
         </a>
         <a href='#' class='btn btn-primary btn-transmit' onclick='window.open("../patient_file/appt_label.php", "_blank").opener = null' onsubmit='return top.restoreSession()'>
@@ -620,7 +620,8 @@ if (!empty($_POST['form_refresh']) || !empty($_POST['form_orderby'])) {
     }
 
 //ALB - Gather PIDs and dates for checked boxes for progress notes
-    if ($_POST['form_progress2']) { ?>
+    var_dump($_POST['form_progress']);
+    if ($_POST['form_progress']) { ?>
         <div id="report_parameters">
             <a href='#' class='css_button' onclick='window.print()'> <span> <?php echo xlt('Print'); ?>
 				</span> </a>
