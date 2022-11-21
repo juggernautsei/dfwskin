e9af6d71bf2e719ff3af60264d2c09a32b9aa873
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-21 13:13:22 -0500

    card expand or hide then record using settings

--- interface/patient_file/summary/demographics.php
+++ interface/patient_file/summary/demographics.php
@@ -340,6 +340,7 @@ $oemr_ui = new OemrUI($arrOeUiSettings);
 
         function toggleIndicator(target, div) {
             // <i id="show_hide" class="fa fa-lg small fa-eye-slash" title="Click to Hide"></i>
+            alert(div.id + " and " + target);
             $mode = $(target).find(".indicator").text();
             if ($mode == <?php echo xlj('collapse'); ?>) {
                 $(target).find(".indicator").text(<?php echo xlj('expand'); ?>);
